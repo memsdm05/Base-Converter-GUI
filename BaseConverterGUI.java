bfed96765f0ee32d96df80029d34e2ce344d6790
Ben Browner <32436024+memsdm05@users.noreply.github.com>
2019-11-06 13:14:03 -0500

    Initial Commit

---
+++ BaseConverterGUI.java
@@ -0,0 +1,45 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Loads FXML File and displays it
+ *
+ * @author Ben Browner
+ * @since 10/23/2019
+ *
+ */
+
+public class BaseConverterGUI extends Application
+{
+    public static void main(String[] args)
+    {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        // Create the FXMLLoader
+        FXMLLoader loader = new FXMLLoader();
+        // Path to the FXML File
+        String fxmlDocPath = "index.fxml";
+        FileInputStream fxmlStream = new FileInputStream(fxmlDocPath);
+
+        TabPane root = (TabPane) loader.load(fxmlStream);
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Ben's Base Converter");
+        stage.show();
+    }
+}
